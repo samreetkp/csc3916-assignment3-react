8e7c55a4a09aef3699bbea8121b0fb0006e4e939
samreetkp <samreetkpadda@gmail.com>
2026-03-08 00:13:09 -0700

    Fixed unused username variable causing build failure

--- src/components/movieheader.js
+++ src/components/movieheader.js
@@ -7,7 +7,6 @@ import { logoutUser } from "../actions/authActions";
 function MovieHeader() {
     const dispatch = useDispatch();
     const loggedIn = useSelector((state) => state.auth.loggedIn);
-    const username = useSelector((state) => state.auth.username);
     const selectedMovie = useSelector((state) => state.movie.selectedMovie);
     
     const logout = () => {
@@ -20,23 +19,33 @@ function MovieHeader() {
                 <Navbar.Brand as={NavLink} to="/">Movie App</Navbar.Brand> 
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto">
-                    <Nav.Link as={NavLink} to="/movielist" disabled={!loggedIn}> 
-                        Movie List
-                    </Nav.Link>
-                    <Nav.Link as={NavLink} to={'/movie/' + (selectedMovie? selectedMovie._id: '')} disabled={!loggedIn}>
-                        Movie Detail
-                    </Nav.Link>
-                    <Nav.Link as={NavLink} to="/signin"> 
-                        {loggedIn? (
-                        <span onClick={logout} style={{ cursor: 'pointer' }}>
-                            Logout
-                        </span>
-                        ): (
-                        'Login'
-                        )}
-                    </Nav.Link>
-                </Nav>
+
+                    <Nav className="ml-auto">
+
+                        <Nav.Link as={NavLink} to="/movielist" disabled={!loggedIn}>
+                            Movie List
+                        </Nav.Link>
+
+                        <Nav.Link 
+                            as={NavLink} 
+                            to={'/movie/' + (selectedMovie ? selectedMovie._id : '')} 
+                            disabled={!loggedIn}
+                        >
+                            Movie Detail
+                        </Nav.Link>
+
+                        <Nav.Link as={NavLink} to="/signin">
+                            {loggedIn ? (
+                                <span onClick={logout} style={{ cursor: 'pointer' }}>
+                                    Logout
+                                </span>
+                            ) : (
+                                'Login'
+                            )}
+                        </Nav.Link>
+
+                    </Nav>
+
                 </Navbar.Collapse>
             </Navbar>
         </div>
